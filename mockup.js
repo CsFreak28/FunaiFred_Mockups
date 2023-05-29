f31165cb7cf956618312b832b3c0af3a9b40863a
CsFreak28 <uwagideon092@gmail.com>
2023-05-29 22:53:27 +0100

    revamped for interactive

--- mockup.js
+++ mockup.js
@@ -41,8 +41,18 @@ app.post("/webhook", (req, res) => {
       let phone_number_id =
         req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
-      let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      console.log(msg_body);
+      let messageType = request.body.entry[0].changes[0].value.messages[0].type;
+      let msg_body =
+        messageType == req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
+      if (messageType == "interactive") {
+        msg_body =
+          req.body.entry[0].changes[0].value.messages[0].interactive.type ===
+          "button_reply"
+            ? req.body.entry[0].changes[0].value.messages[0].interactive
+                .button_reply.title
+            : req.body.entry[0].changes[0].value.messages[0].interactive
+                .list_reply.title;
+      }
       if (msg_body == "restart") {
         requestCount = 0;
         arrayOfFunctions[requestCount](phone_number_id, from);
