894f3964ff81ace4aef5ef422c1d01100b2a1a06
CsFreak28 <uwagideon092@gmail.com>
2023-05-31 22:44:00 +0100

    removed awaits

--- announcementMockup.js
+++ announcementMockup.js
@@ -3,7 +3,7 @@ async function sendGreeting(phone_number_id, from) {
   console.log("started");
   const token = process.env.WHATSAPP_TOKEN;
   //send greeting first
-  await axios({
+  axios({
     method: "POST", // Required, HTTP method, a string, e.g. POST, GET
     url:
       "https://graph.facebook.com/v16.0/" +
@@ -18,114 +18,116 @@ async function sendGreeting(phone_number_id, from) {
       },
     },
     headers: { "Content-Type": "application/json" },
-  });
-  await axios({
-    method: "POST",
-    url: "https://graph.facebook.com/v15.0/" + phone_number_id + "/messages",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-    data: {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      to: from,
-      type: "interactive",
-      interactive: {
-        type: "list",
-        header: {
-          type: "text",
-          text: "THINGS I CAN DO ",
-        },
-        body: {
-          text: "This is a list of things I can help you do right now, \n in a future update I'm sure I'll be able to help more",
-        },
-        action: {
-          button: "COMMAND LIST",
-          sections: [
-            {
-              title: "PAYMENTS 💰",
-              rows: [
-                {
-                  title: "Make A Payment",
-                  description: "I want to make a payment.",
-                  id: "23",
-                },
-                {
-                  title: "Payment Reciepts",
-                  description: "I want to get the reciept of a payment I made.",
-                  id: "24",
-                },
-              ],
-            },
-            {
-              title: "INFORMATION 📰",
-              rows: [
-                {
-                  title: "Course Information",
-                  description:
-                    "I want to get information on things happening in CSC department",
-                  id: "25",
-                },
-                {
-                  title: "School Information",
-                  description:
-                    "I want to get information on things happening in School",
-                  id: "26",
-                },
-              ],
-            },
-            {
-              title: "ANNOUNCEMENTS 📢",
-              rows: [
-                {
-                  title: "Make An Announcement",
-                  description: "I want to make an announcement.",
-                  id: "56",
-                },
-                {
-                  title: "Track An Announcement",
-                  description: "I want to manage an announcement",
-                  id: "65",
-                },
-              ],
-            },
-            {
-              title: "ATTENDANCE (✨)",
-              rows: [
-                {
-                  title: "Take Attendance",
-                  description: "Take my attendance",
-                  id: "30",
-                },
-                {
-                  title: "Attendance Record",
-                  description: "Get my attendance record",
-                  id: "31",
-                },
-              ],
-            },
-            {
-              title: "ACADEMIC STUFF 📚",
-              rows: [
-                {
-                  title: "My Result",
-                  description: "Get My Result",
-                  id: "32",
-                },
-              ],
-            },
-          ],
+  }).then(() => {
+    axios({
+      method: "POST",
+      url: "https://graph.facebook.com/v15.0/" + phone_number_id + "/messages",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: from,
+        type: "interactive",
+        interactive: {
+          type: "list",
+          header: {
+            type: "text",
+            text: "THINGS I CAN DO ",
+          },
+          body: {
+            text: "This is a list of things I can help you do right now, \n in a future update I'm sure I'll be able to help more",
+          },
+          action: {
+            button: "COMMAND LIST",
+            sections: [
+              {
+                title: "PAYMENTS 💰",
+                rows: [
+                  {
+                    title: "Make A Payment",
+                    description: "I want to make a payment.",
+                    id: "23",
+                  },
+                  {
+                    title: "Payment Reciepts",
+                    description:
+                      "I want to get the reciept of a payment I made.",
+                    id: "24",
+                  },
+                ],
+              },
+              {
+                title: "INFORMATION 📰",
+                rows: [
+                  {
+                    title: "Course Information",
+                    description:
+                      "I want to get information on things happening in CSC department",
+                    id: "25",
+                  },
+                  {
+                    title: "School Information",
+                    description:
+                      "I want to get information on things happening in School",
+                    id: "26",
+                  },
+                ],
+              },
+              {
+                title: "ANNOUNCEMENTS 📢",
+                rows: [
+                  {
+                    title: "Make An Announcement",
+                    description: "I want to make an announcement.",
+                    id: "56",
+                  },
+                  {
+                    title: "Track An Announcement",
+                    description: "I want to manage an announcement",
+                    id: "65",
+                  },
+                ],
+              },
+              {
+                title: "ATTENDANCE (✨)",
+                rows: [
+                  {
+                    title: "Take Attendance",
+                    description: "Take my attendance",
+                    id: "30",
+                  },
+                  {
+                    title: "Attendance Record",
+                    description: "Get my attendance record",
+                    id: "31",
+                  },
+                ],
+              },
+              {
+                title: "ACADEMIC STUFF 📚",
+                rows: [
+                  {
+                    title: "My Result",
+                    description: "Get My Result",
+                    id: "32",
+                  },
+                ],
+              },
+            ],
+          },
         },
       },
-    },
+    });
   });
 }
 async function writeAnnouncement(phone_number_id, from) {
   console.log("sujper");
   const token = process.env.WHATSAPP_TOKEN;
   //send greeting first
-  await axios({
+  axios({
     method: "POST", // Required, HTTP method, a string, e.g. POST, GET
     url:
       "https://graph.facebook.com/v16.0/" +
@@ -144,7 +146,7 @@ async function writeAnnouncement(phone_number_id, from) {
 }
 async function selectGroup(phone_number_id, from) {
   const token = process.env.WHATSAPP_TOKEN;
-  await axios({
+  axios({
     method: "POST",
     url: "https://graph.facebook.com/v15.0/" + phone_number_id + "/messages",
     headers: {
@@ -217,7 +219,7 @@ async function selectGroup(phone_number_id, from) {
 async function announcementTime(phone_number_id, from) {
   const token = process.env.WHATSAPP_TOKEN;
   //send greeting first
-  await axios({
+  axios({
     method: "POST", // Required, HTTP method, a string, e.g. POST, GET
     url:
       "https://graph.facebook.com/v16.0/" +
@@ -232,45 +234,47 @@ async function announcementTime(phone_number_id, from) {
       },
     },
     headers: { "Content-Type": "application/json" },
-  });
-  await axios({
-    method: "POST",
-    url: "https://graph.facebook.com/v15.0/" + phone_number_id + "/messages",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    data: {
-      messaging_product: "whatsapp",
-      recipient_type: "individual",
-      to: from,
-      type: "interactive",
-      interactive: {
-        type: "button",
-        body: {
-          text: "With the deadline being Friday, I will send this announcement right away and three more times to ensure that everyone follows the instruction \n are you okay with that ?",
-        },
-        action: {
-          buttons: [
-            {
-              type: "reply",
-              reply: {
-                title: "Yes, I am",
-                id: 23,
+  }).then(() => {
+    axios({
+      method: "POST",
+      url: "https://graph.facebook.com/v15.0/" + phone_number_id + "/messages",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        messaging_product: "whatsapp",
+        recipient_type: "individual",
+        to: from,
+        type: "interactive",
+        interactive: {
+          type: "button",
+          body: {
+            text: "With the deadline being Friday, I will send this announcement right away and three more times to ensure that everyone follows the instruction \n are you okay with that ?",
+          },
+          action: {
+            buttons: [
+              {
+                type: "reply",
+                reply: {
+                  title: "Yes, I am",
+                  id: 23,
+                },
               },
-            },
-            {
-              type: "reply",
-              reply: {
-                title: "No, I'm not",
-                id: 26,
+              {
+                type: "reply",
+                reply: {
+                  title: "No, I'm not",
+                  id: 26,
+                },
               },
-            },
-          ],
+            ],
+          },
         },
       },
-    },
+    });
   });
+
   const url = "https://www.bulksmsnigeria.com/api/v2/sms "; // Replace with your API endpoint URL
 
   const postData = {
@@ -292,7 +296,7 @@ async function announcementTime(phone_number_id, from) {
 async function confirmation(phone_number_id, from) {
   const token = process.env.WHATSAPP_TOKEN;
 
-  await axios({
+  axios({
     method: "POST", // Required, HTTP method, a string, e.g. POST, GET
     url:
       "https://graph.facebook.com/v16.0/" +
