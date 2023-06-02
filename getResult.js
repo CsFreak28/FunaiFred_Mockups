c5de8a3a489f3b06a573e9f540fdb07b637c9a69
CsFreak28 <uwagideon092@gmail.com>
2023-06-02 10:04:39 +0100

    updated to property in text message

--- getResult.js
+++ getResult.js
@@ -1,5 +1,22 @@
 const getResultFunctions = {
-  hello: () => {},
+  hello: () => {
+    axios({
+      method: "POST", // Required, HTTP method, a string, e.g. POST, GET
+      url:
+        "https://graph.facebook.com/v16.0/" +
+        phone_number_id +
+        "/messages?access_token=" +
+        token,
+      data: {
+        messaging_product: "whatsapp",
+        to: from,
+        text: {
+          body: `Hey ${name}, \n I'm the *BETA* version of FUNAI FRED 🤩, right now all I can do for you `,
+        },
+      },
+      headers: { "Content-Type": "application/json" },
+    });
+  },
   yourPassword: () => {},
   yourUserName: () => {},
   getResults: () => {},
